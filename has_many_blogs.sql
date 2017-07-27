126ce94dd2494d1f76e19d4c867c1bfb50ad445c
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-27 09:55:47 -1000

    created all tables

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -6,6 +6,35 @@
  CREATE USER has_many_user;
  CREATE DATABASE has_many_blogs;
 
---Run the provided scripts/car_models.sql script on the indexed_cars database
-\c has_many_blogs.sql
-\i scripts/blog_data.sql
+--Connect to the newly created database
+\c has_many_blogs;
+
+--Users
+CREATE TABLE users (
+  id SERIAL NOT NULL PRIMARY KEY,
+  username VARCHAR(90) NOT NULL,
+  first_name VARCHAR(90) DEFAULT NULL,
+  last_name VARCHAR(90) DEFAULT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+--Posts
+CREATE TABLE posts (
+  id SERIAL NOT NULL,
+  title VARCHAR(180) DEFAULT NULL,
+  url VARCHAR(510) DEFAULT NULL,
+  content TEXT DEFAULT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+--Comments
+CREATE TABLE posts (
+  id SERIAL NOT NULL,
+  title VARCHAR(180) DEFAULT NULL,
+  url VARCHAR(510) DEFAULT NULL,
+  content TEXT DEFAULT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
