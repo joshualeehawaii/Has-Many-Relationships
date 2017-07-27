8754491f64ae833c970202a2cdf19cfb24ddae02
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-27 10:55:18 -1000

    need code review

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -4,7 +4,7 @@
 
 --Create a new database named has_many_blogs owned by has_many_user
  CREATE USER has_many_user;
- CREATE DATABASE has_many_blogs;
+ CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
 
 --Connect to the newly created database
 \c has_many_blogs;
@@ -20,6 +20,7 @@ CREATE TABLE users (
 );
 
 --Posts
+DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
   id SERIAL NOT NULL,
   title VARCHAR(180) DEFAULT NULL,
@@ -27,14 +28,19 @@ CREATE TABLE posts (
   content TEXT DEFAULT NULL,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+  --user_id INTEGER REFERENCES users
 );
 
 --Comments
-CREATE TABLE posts (
+DROP TABLE IF EXISTS comments;
+CREATE TABLE comments (
   id SERIAL NOT NULL,
-  title VARCHAR(180) DEFAULT NULL,
-  url VARCHAR(510) DEFAULT NULL,
-  content TEXT DEFAULT NULL,
+  body VARCHAR(510) DEFAULT NULL,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+  --user_id INTEGER REFERENCES users,
+  --posts_id INTEGER REFERENCES posts
 );
+
+--data fro the tables
+\i scripts/blog_data.sql;
