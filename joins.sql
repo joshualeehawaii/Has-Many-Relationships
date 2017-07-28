7e90b140a5cef7ca227fa3b4d51d8f33d93869c3
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-27 18:51:10 -1000

    making progress

--- joins.sql
+++ joins.sql
@@ -1,5 +1,3 @@
-
-
 --1. Create a query to get all fields from the users table
 SELECT * FROM users;
 
@@ -9,19 +7,26 @@ WHERE user_id = 100;
 
 --3. Create a query to get all posts fields, the user's first name, and the user's last name, from the posts table where the user's id is 200
 SELECT posts.*, users.first_name, users.last_name
-FROM posts INNER JOIN users ON posts.user_id = users.id
+FROM posts JOIN users ON posts.user_id = users.id
 WHERE user_id = 200;
 
 --4. Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
-
+SELECT posts.*, users.username
+FROM posts JOIN users ON posts.user_id = users.id
+WHERE users.first_name = 'Norene' AND users.last_name = 'Schmitt';
 
 --5. Create a query to get usernames from the users table where the user has created a post after January 1, 2015
-
+SELECT username
+FROM users JOIN posts ON posts.user_id = users.id
+WHERE posts.created_at > '2015-01-01';
 
 --6. Create a query to get the post title, post content, and user's username where the user who created the post joined before January 1, 2015
-
+SELECT posts.title, posts.content, users.username
+FROM users JOIN
+WHERE users.created_at < '2015-01-01';
 
 --7. Create a query to get the all rows in the comments table, showing post title (aliased as 'Post Title'), and the all the comment's fields
+
 
 
 --8. Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created before January 1, 2015
