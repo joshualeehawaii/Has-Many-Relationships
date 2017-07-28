92c8b5143cc3e46678ebfc90d3c3ef137bc4c4f5
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-27 18:24:11 -1000

    adding number 3

--- joins.sql
+++ joins.sql
@@ -3,16 +3,14 @@
 --1. Create a query to get all fields from the users table
 SELECT * FROM users;
 
-
 --2. Create a query to get all fields from the posts table where the user_id is 100
 SELECT * FROM posts
-WHERE users_id = 100;
+WHERE user_id = 100;
 
 --3. Create a query to get all posts fields, the user's first name, and the user's last name, from the posts table where the user's id is 200
-SELECT title,  FROM posts
+SELECT posts.*, users.first_name, users.last_name
+FROM posts INNER JOIN users ON posts.user_id = users.id
 WHERE user_id = 200;
-
-
 
 --4. Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
 
